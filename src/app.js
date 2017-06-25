372513c1e212ed90edbfa13d3d4501c66cc20c81
Guido Cecilio <guido.cecilio@propylon.com>
2017-06-25 01:43:49 +0100

    First commit.

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import {PLATFORM} from 'aurelia-pal';
+
+export class App {
+  configureRouter(config, router) {
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: PLATFORM.moduleName('./welcome'),      nav: true, title: 'Welcome' },
+      { route: 'users',         name: 'users',        moduleId: PLATFORM.moduleName('./users'),        nav: true, title: 'Github Users' },
+      { route: 'child-router',  name: 'child-router', moduleId: PLATFORM.moduleName('./child-router'), nav: true, title: 'Child Router' }
+    ]);
+
+    this.router = router;
+  }
+}
